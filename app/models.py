d772e0726b421a53d8481b1dd8b30529d1439906
Student <student@donation-2.local>
2019-11-13 15:06:53 -0600

    Created all my models and Form

--- app/models.py
+++ app/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from datetime import datetime
+from django import forms
 
-# Create your models here.
+
+class DogProduct(models.Model):
+    name = models.TextField()
+    product_type = models.TextField()
+    dog_size = models.TextField()
+    price = models.FloatField()
+    quantity = models.IntegerField()
+
+
+class Purchase(models.Model):
+    dog_product = models.ForeignKey(DogProduct, on_delete=models.PROTECT)
+    purchased_at = models.DateTimeField()
+
+
+class DogTag(models.Model):
+    owner_name = models.TextField()
+    dog_name = models.TextField()
+    dog_birthday = models.DateField()
+
+
+class NewDogTagForm(forms.Form):
+    owner_name = forms.CharField()
+    dog_name = forms.CharField()
+    dog_birthday = forms.DateField()
