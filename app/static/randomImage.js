82be75ca1e21de06022a1af126180b406e21fee6
Student <student@donation-2.local>
2019-11-15 15:53:00 -0600

    Created & linked JS page for random dog images

---
+++ app/static/randomImage.js
@@ -0,0 +1,23 @@
+function getRandomImage() {
+    var images = ["https://images.unsplash.com/photo-1518020382113-a7e8fc38eac9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60",
+        "https://images.unsplash.com/photo-1517423440428-a5a00ad493e8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=687&q=80",
+        "https://images.unsplash.com/photo-1505628346881-b72b27e84530?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60",
+        "https://images.unsplash.com/photo-1517849845537-4d257902454a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60",
+        "https://images.unsplash.com/photo-1558788353-f76d92427f16?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60",
+        "https://images.unsplash.com/photo-1497994187231-bc847a69dc76?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60",
+        "https://images.unsplash.com/photo-1509005084666-3cbc75184cbb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60",
+        "https://images.unsplash.com/photo-1543466835-00a7907e9de1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60",
+        "https://images.unsplash.com/photo-1526440847959-4e38e7f00b04?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60",
+    ];
+    var image = images[Math.floor(Math.random() * images.length)];
+
+    return image;
+}
+
+function displayRandomImage() {
+    var htmlImages = document.getElementsByClassName("dog-image");
+    for (const image of htmlImages) {
+        image.src = getRandomImage();
+    }
+}
+displayRandomImage();
